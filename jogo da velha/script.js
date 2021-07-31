a3d36df144dfe5494e5947d82ee9c0c2cfd672fa
Sergio81alves <76402373+Sergio81alves@users.noreply.github.com>
2021-07-30 23:16:36 -0300

    frist

---
+++ jogo da velha/script.js
@@ -0,0 +1,20 @@
+//Dados iníciais initial Data
+let square = {
+    a1: '', a2: '', a3: '',
+    b1: '', b2: '', b3: '',
+    c1: '', c2: '', c3: '',
+};
+let player = '';
+let warning = '';
+let playing = false;
+
+//Eventos events
+document.querySelector('.reset').addEventListener('click',reset);
+
+
+//Funções function
+function reset() {
+    warning = '';
+
+    let random = Math.floor(math.random()*2);
+}
